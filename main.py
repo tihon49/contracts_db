92005e0183f4e49fd63cf906dc741fca6871b0ce
Tihon49 <tihon49@gmail.com>
2020-11-17 18:16:17 +0300

    SQLAlchemy added

--- main.py
+++ main.py
@@ -1,185 +1,57 @@
-import psycopg2 as pg
+import sqlalchemy as sa
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm.session import sessionmaker
+
+engine = sa.create_engine('postgresql+psycopg2://contracts_admin:1234@localhost:5432/contracts_db')  # 60557
+connection = engine.connect()
+Base = declarative_base()
+session = sessionmaker(bind=engine)()
 
 
-def create_agent_table(cur):
-    """Создание таблицы Agents"""
+class User(Base):
+    __tablename__ = 'users'
 
-    # создаем таблицу Agents, если такой еще нет
-    cur.execute("""
-        create table if not exists Agents(
-            id serial primary key,
-            name varchar(48) UNIQUE not null    
-        );
-    """)
+    id = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column(sa.String)
+    fullname = sa.Column(sa.String)
+    nickname = sa.Column(sa.String)
 
-
-def create_contract_table(cur):
-    """
-    создание таблицы Contracts которая содержит
-    данные о договоре с привязкой к контрагенту
-    """
-
-    # создаем таблицу Contracts, если такой еще нет
-    cur.execute("""
-        create table if not exists Contracts(
-            id serial primary key,
-            agent_id integer references agents(id) on delete cascade,
-            number varchar(24) not null,
-            description varchar(128)
-        );
-    """)
+    def __repr__(self):
+        return "<User(name='%s', fullname='%s', nickname='%s')>" % (self.name, self.fullname, self.nickname)
 
 
-def create_bills_table(cur):
-    """создание таблицы счетов Bills"""
+class Agent(Base):
+    __tablename__ = 'agents'
+    id = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column(sa.String(48))
 
-    cur.execute("""
-        create table if not exists Bills(
-            id serial primary key,
-            contract_id integer references contracts(id) on delete cascade,
-            bill_number varchar(24) not null,
-            act_number varchar(24) not null,
-            sum integer not null,
-            bill_date date not null,
-            act_date date null
-        );
-    """)
-
-
-def insert_new_agent(agent_name, cur):
-    """
-    доабавление контрагента в таблицу Agents
-
-    agent_name: string с именем добовляемого контрагента
-    """
-
-    try:
-        cur.execute(f"insert into Agents(name) values(%s)", (agent_name,))
-    except pg.Error as e:
-        print(e)
-        # print('Данныйы контрагент уже существует в БД')
-
-
-def insert_new_contract(agent_id, name, description, cur):
-    """
-    добавление нового договора в таблицу Contracts
-    с привязкой к id контрагента из таблицы Agents
-
-    agent_id: integer id контрагента из таблицы Agents
-    name: string номер договора
-    description: string краткое описание договора
-    """
-
-    # вставляем в таблицу Contracts новый договор с привязкой к контрагенту
-    cur.execute(f"insert into Contracts(agent_id, number, description) values(%s, %s, %s)",
-                (agent_id, name, description))
-
-
-def show_all_agents(cur):
-    """Вывод всех контрагентов из таблицы Agents"""
-
-    cur.execute("select * from Agents")
-    agents = cur.fetchall()
-
-    for agent in agents:
-        id_ = agent[0]
-        name = agent[1]
-        print(f'id: {id_} name: {name}')
-    print()
-
-
-def show_all_contracts(cur):
-    """Вывод всех договоров из таблицы Contracts"""
-
-    cur.execute("select * from Contracts")
-    contracts = cur.fetchall()
-
-    for contract in contracts:
-        id_ = contract[0]
-        agent_id = contract[1]
-        contract_number = contract[2]
-        description = contract[3]
-        print(f'id: {id_}  agent_id:{agent_id}  contract_number: {contract_number}\n'
-              f'description: {description}\n')
-    print()
-
-
-def show_all_bills(cur):
-    """Вывод всех счетов"""
-
-    cur.execute('select * from Bills')
-    all_bills = cur.fetchall()
-
-    for bill in all_bills:
-        print(bill)
-    print()
-
-
-def show_all(cur):
-    """Вывод сводной (JOIN) информации из таблиц Agents и Contracts"""
-
-    cur.execute("""
-        select c.id, name, agent_id, number, description 
-        from contracts as c
-        join Agents a
-        on c.agent_id = a.id
-    """)
-    relations = cur.fetchall()
-
-    for item in relations:
-        id_ = item[0]
-        agent_name = item[1]
-        agent_id = item[2]
-        contract_number = item[4]
-        description = item[5]
-        print(f'id: {id_}, agent_name: {agent_name}, agent_id: {agent_id}\n'
-              f'description: {description}\n, '
-              f'contract_number: {contract_number}')
-        print()
-    print()
+    def __repr__(self):
+        return f'<Agent(name="{self.name}")>'
 
 
 def main():
-    conn = pg.connect(dbname='contracts_db', user='contracts_admin', password='1234')
-    conn.set_session(autocommit=True)  # чтобы не делать conn.commit() каждый раз
-    cur = conn.cursor()
-
-    create_agent_table(cur)
-    create_contract_table(cur)
-    create_bills_table(cur)
+    Base.metadata.create_all(engine)
 
     while True:
-        choose = input('Выберите действие:\n'
-                       '1. Добавить контрагента\n'
-                       '2. Добавить договор\n'
-                       '3. Посмотреть список контрагентов\n'
-                       '4. Посмотреть список договоров\n'
-                       '5. Посмотреть все счета\n'
-                       '6. Вывести всю информацию\n'
-                       )
+        print('1. создать нового пользователя\n'
+              '2. показать всех пользователей\n'
+              'q. выйти\n')
 
-        if choose == 'q':
-            break
-        elif choose == '1':
-            name = input('Введите имя контрагета: ')
-            insert_new_agent(name, cur)
+        choose = input()
+
+        if choose == '1':
+            name = input('Enter a name: ')
+            fullname = input('Enter a fullname: ')
+            new_user = User(name=name, fullname=fullname)
+            session.add(new_user)
         elif choose == '2':
-            agent_id = int(input('Введите id контрагента: '))
-            number = input('Введите номер договора: ')
-            description = input('Введите описание договора: ')
-            insert_new_contract(agent_id, number, description, cur)
-        elif choose == '3':
-            show_all_agents(cur)
-        elif choose == '4':
-            show_all_contracts(cur)
-        elif choose == '5':
-            show_all_bills(cur)
-        elif choose == '6':
-            show_all(cur)
-        else:
-            print('Введена не верная команда. Попробуйте еще раз\n')
+            q = session.query(Agent).all()
+            for a in q:
+                print(a)
+        elif choose == 'q':
+            break
 
-    conn.close()
+
 
 
 if __name__ == '__main__':
