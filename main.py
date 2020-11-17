cf8b1fc1fa70aca33857ebac9129765b218532aa
Tihon49 <tihon49@gmail.com>
2020-11-17 20:40:10 +0300

    SQLAlchemy added

--- main.py
+++ main.py
@@ -8,7 +8,29 @@ Base = declarative_base()
 session = sessionmaker(bind=engine)()
 
 
+def choose_table_to_show(choose_number:str):
+    """выдаем queryset указанной таблицы"""
+
+    if choose_number == '1':
+        table = Agent
+    elif choose_number == '2':
+        table = Contracts
+    elif choose_number == '3':
+        table = User
+    elif choose_number == 'q':
+        return
+    else:
+        print('Не верная команда\n')
+        return
+
+    query = session.query(table).all()
+    for item in query:
+        print(item)
+
+
 class User(Base):
+    """таблица users"""
+
     __tablename__ = 'users'
 
     id = sa.Column(sa.Integer, primary_key=True)
@@ -21,6 +43,8 @@ class User(Base):
 
 
 class Agent(Base):
+    """таблица agents"""
+
     __tablename__ = 'agents'
     id = sa.Column(sa.Integer, primary_key=True)
     name = sa.Column(sa.String(48))
@@ -29,12 +53,26 @@ class Agent(Base):
         return f'<Agent(name="{self.name}")>'
 
 
+class Contracts(Base):
+    """таблица с договорами contracts"""
+
+    __tablename__ = 'contracts'
+
+    id = sa.Column(sa.Integer, primary_key=True)
+    agent_id = sa.Column(sa.Integer, sa.ForeignKey('agent.id'), nullable=False)
+    number = sa.Column(sa.String(24), nullable=False)
+    description = sa.Column(sa.String(128), nullable=False)
+
+    def __repr__(self):
+        return f'<Contract(id="{self.id}", agent_id="{self.agent_id}", number="{self.number}"\ndescription: {self.description})>\n'
+
+
 def main():
-    Base.metadata.create_all(engine)
+    Base.metadata.create_all(engine)  # создание всех таблиц
 
     while True:
         print('1. создать нового пользователя\n'
-              '2. показать всех пользователей\n'
+              '2. Поиск по таблицам\n'
               'q. выйти\n')
 
         choose = input()
@@ -42,16 +80,15 @@ def main():
         if choose == '1':
             name = input('Enter a name: ')
             fullname = input('Enter a fullname: ')
-            new_user = User(name=name, fullname=fullname)
+            nickname = input('Enter a nickname: ')
+            new_user = User(name=name, fullname=fullname, nickname=nickname)
             session.add(new_user)
+            session.commit()
         elif choose == '2':
-            q = session.query(Agent).all()
-            for a in q:
-                print(a)
+            choose_number = input('\n1. Все агенты\n2. Все договоры\n3. Все пользователи\nq. Назад')
+            choose_table_to_show(choose_number)
         elif choose == 'q':
             break
-
-
 
 
 if __name__ == '__main__':
