1ace0a6322704e128d04f9200c8eccb55d7ffb33
Tihon49 <tihon49@gmail.com>
2020-11-19 15:14:53 +0300

    constrain relations

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sqlalchemy as db
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm.session import sessionmaker
 
-from models import Agent, Contract, Bill, create_tales, delete_all_tables
+from models import Agent, Contract, create_tales, delete_all_tables
 
 engine = db.create_engine('postgresql+psycopg2://contracts_admin:1234@localhost:5432/contracts_db')
 connection = engine.connect()
@@ -53,25 +53,6 @@ def add_contract():
     print(f'Договор №{number} добавлен\n')
 
 
-def add_bill():
-    """добавление счета"""
-
-    contract_id = int(input('id Договора: '))
-    bill_number = input('Номер счета: ')
-    act_number = input('Номер акта: ')
-    bill_sum = int(input('Сумма счета: '))
-    act_sum = int(input('Сумма акта: '))
-
-    new_bill = Bill(contract_id=contract_id, bill_number=bill_number,
-                    act_number=act_number, bill_sum=bill_sum, act_sum=act_sum)
-    session.add(new_bill)
-    session.commit()
-
-    bills_query = session.query(Bill).filter_by(contract_id=contract_id, bill_number=bill_number).all()
-    print(f'Добавлен счет {bills_query[0]}')
-    # TODO: проверку данных на валидность
-
-
 def choose_table_to_show(choose_number):
     """выдаем queryset указанной таблицы"""
 
@@ -79,15 +60,11 @@ def choose_table_to_show(choose_number):
         print(session.query(Agent).all())
     elif choose_number == '2':
         print(session.query(Contract).all())
-    elif choose_number == '3':
-        print(session.query(Bill).all())
     elif choose_number == 'q':
         return
     else:
         print('Не верная команда\n')
         return
-
-
 
 
 def main():
@@ -104,14 +81,11 @@ def main():
         if choose == '1':
             choose_number = input('\n1. Добавить агента\n'
                                   '2. Добавить договор\n'
-                                  '3. Добавить счет\n'
                                   'q. Назад\n')
             if choose_number == '1':
                 add_agent()
             elif choose_number == '2':
                 add_contract()
-            elif choose_number == '3':
-                add_bill()
             elif choose_number == 'q':
                 pass
             else:
@@ -120,7 +94,6 @@ def main():
         elif choose == '2':
             choose_number = input('\n1. Все агенты\n'
                                   '2. Все договоры\n'
-                                  '3. Все счета\n'
                                   'q. Назад\n')
             choose_table_to_show(choose_number)
         elif choose == '3':
--- models.py
+++ models.py
@@ -15,7 +15,7 @@ class Agent(Base):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(48), unique=True)
-    contracts = orm.relation('Contract', backref='agent')
+    contracts = orm.relationship('Contract', backref='agent')
 
     def __repr__(self):
         return f'<Agent(id="{self.id}", name="{self.name}")>'
@@ -26,35 +26,15 @@ class Contract(Base):
 
     __tablename__ = 'contracts'
 
-    id = db.Column(db.Integer, primary_key=True)
     agent_id = db.Column(db.Integer, db.ForeignKey('agents.id'), nullable=False)
     number = db.Column(db.String(24), nullable=False)
     description = db.Column(db.String(128), nullable=False)
-    bills = orm.relation('Bill', backref='contract')
+
+    __table_args__ = (db.PrimaryKeyConstraint(agent_id, number),)
 
     def __repr__(self):
-        return f'<Contract(id="{self.id}", agent_id="{self.agent_id}", number="{self.number}", ' \
+        return f'<Contract(agent_id="{self.agent_id}", number="{self.number}", ' \
                f'description: {self.description})>\n'
-
-
-class Bill(Base):
-    """таблица счетов bills"""
-
-    __tablename__ = 'bills'
-
-    id = db.Column(db.Integer, primary_key=True)
-    contract_id = db.Column(db.Integer, db.ForeignKey('contracts.id'), nullable=False)
-    bill_number = db.Column(db.String(24), nullable=False)
-    act_number = db.Column(db.String(24), nullable=False)
-    bill_sum = db.Column(db.Integer, nullable=False)
-    act_sum = db.Column(db.Integer, nullable=False)
-    bill_date = db.Column(db.Date)
-    act_date = db.Column(db.Date)
-
-    def __repr__(self):
-        return f'<Bill(id="{self.id}", contract_id="{self.contract_id}", bill_number="{self.bill_number}", ' \
-               f'act_number="{self.act_number}", bill_sum="{self.bill_sum}", act_sum="{self.act_sum}", ' \
-               f'bill_date="{self.bill_date}", act_date="{self.act_date}")>'
 
 
 def create_tales():
