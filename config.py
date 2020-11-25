99ec722758c00f09332c2e268f66b730346ee863
Tihon49 <tihon49@gmail.com>
2020-11-25 17:05:55 +0300

    научился создавать строки в таблице

---
+++ config.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'untitled.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1073, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(242, 497, 591, 51))
+        self.pushButton.setObjectName("pushButton")
+        self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
+        self.tableWidget.setGeometry(QtCore.QRect(20, 10, 1031, 471))
+        self.tableWidget.viewport().setProperty("cursor", QtGui.QCursor(QtCore.Qt.ArrowCursor))
+        self.tableWidget.setContextMenuPolicy(QtCore.Qt.DefaultContextMenu)
+        self.tableWidget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+        self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setColumnCount(4)
+        self.tableWidget.setRowCount(1)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setVerticalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(1, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(2, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(3, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setItem(0, 0, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setItem(0, 1, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setItem(0, 2, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setItem(0, 3, item)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1073, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Done"))
+        item = self.tableWidget.verticalHeaderItem(0)
+        item.setText(_translate("MainWindow", "1"))
+        item = self.tableWidget.horizontalHeaderItem(0)
+        item.setText(_translate("MainWindow", "id"))
+        item = self.tableWidget.horizontalHeaderItem(1)
+        item.setText(_translate("MainWindow", "Название"))
+        item = self.tableWidget.horizontalHeaderItem(2)
+        item.setText(_translate("MainWindow", "№ Договора"))
+        item = self.tableWidget.horizontalHeaderItem(3)
+        item.setText(_translate("MainWindow", "Описание"))
+        __sortingEnabled = self.tableWidget.isSortingEnabled()
+        self.tableWidget.setSortingEnabled(False)
+        item = self.tableWidget.item(0, 0)
+        item.setText(_translate("MainWindow", "id"))
+        item = self.tableWidget.item(0, 1)
+        item.setText(_translate("MainWindow", "agent_id"))
+        item = self.tableWidget.item(0, 2)
+        item.setText(_translate("MainWindow", "contract_name"))
+        item = self.tableWidget.item(0, 3)
+        item.setText(_translate("MainWindow", "contract_description"))
+        self.tableWidget.setSortingEnabled(__sortingEnabled)
--- main.py
+++ main.py
@@ -1,65 +1,50 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QGridLayout, QWidget, QTableWidget, QTableWidgetItem
-from PyQt5.QtCore import QSize, Qt
-
-# Наследуемся от QMainWindow
-
-import contracts
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from config import *
+from contracts import show_full_data
 
 
-class MainWindow(QMainWindow):
-    # Переопределяем конструктор класса
-    def __init__(self):
-        # Обязательно нужно вызвать метод супер класса
-        QMainWindow.__init__(self)
+class MyWindow(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
-        self.setMinimumSize(QSize(1200, 400))  # Устанавливаем размеры
-        self.setWindowTitle("Работа с QTableWidget")  # Устанавливаем заголовок окна
-        central_widget = QWidget(self)  # Создаём центральный виджет
-        self.setCentralWidget(central_widget)  # Устанавливаем центральный виджет
+        self.ui.pushButton.setStyleSheet('border: 2 solid green; border-radius: 20px;')
 
-        grid_layout = QGridLayout()  # Создаём QGridLayout
-        central_widget.setLayout(grid_layout)  # Устанавливаем данное размещение в центральный виджет
+        # получаем данные для вывода в таблице
+        self.data = show_full_data()
+        self.ui.tableWidget.setRowCount(len(self.data))
 
-        data = contracts.show_full_data()  # получаем данные из БД
-        table = QTableWidget(self)  # Создаём таблицу
-        table.setColumnCount(4)  # Устанавливаем 4 колонки
-        table.setRowCount(len(data))  # строки в соответствии с кол-вом данных
+        # сначала создаем необходимое кол-во рядов согласно кол-ву полученных данных
+        for i in range(len(self.data)):
+            item = QtWidgets.QTableWidgetItem()
+            self.ui.tableWidget.setItem(i, 0, item)
+            item = QtWidgets.QTableWidgetItem()
+            self.ui.tableWidget.setItem(i, 1, item)
+            item = QtWidgets.QTableWidgetItem()
+            self.ui.tableWidget.setItem(i, 2, item)
+            item = QtWidgets.QTableWidgetItem()
+            self.ui.tableWidget.setItem(i, 3, item)
 
-        # Устанавливаем заголовки таблицы
-        table.setHorizontalHeaderLabels(["id", "Названиве", "№ договора", "Описание"])
-
-        # Устанавливаем всплывающие подсказки на заголовки
-        table.horizontalHeaderItem(0).setToolTip("Column 1 ")
-        table.horizontalHeaderItem(1).setToolTip("Column 2 ")
-        table.horizontalHeaderItem(2).setToolTip("Column 3 ")
-        table.horizontalHeaderItem(3).setToolTip("Column 4 ")
-
-        # Устанавливаем выравнивание на заголовки
-        table.horizontalHeaderItem(0).setTextAlignment(Qt.AlignHCenter)
-        table.horizontalHeaderItem(1).setTextAlignment(Qt.AlignHCenter)
-        table.horizontalHeaderItem(2).setTextAlignment(Qt.AlignHCenter)
-        table.horizontalHeaderItem(3).setTextAlignment(Qt.AlignHCenter)
-
-        # заполняем строки
-        row = 0
-        for agent, contract in data:
-            print(agent, contract)
-            table.setItem(row, 0, QTableWidgetItem(str(agent.id)))
-            table.setItem(row, 1, QTableWidgetItem(agent.name))
-            table.setItem(row, 2, QTableWidgetItem(contract.number))
-            table.setItem(row, 3, QTableWidgetItem(contract.description))
-            row += 1
-
-        # делаем ресайз колонок по содержимому
-        table.resizeColumnsToContents()
-
-        grid_layout.addWidget(table, 0, 0)  # Добавляем таблицу в сетку
+        # теперь добавляем в каждую стоку полученные ранее данные
+        row_number = 0
+        for agent, contract in self.data:
+            item = self.ui.tableWidget.item(row_number, 0)
+            item.setText(str(agent.id))
+            item = self.ui.tableWidget.item(row_number, 1)
+            item.setText(agent.name)
+            item = self.ui.tableWidget.item(row_number, 2)
+            item.setText(contract.number)
+            item = self.ui.tableWidget.item(row_number, 3)
+            item.setText(contract.description)
+            row_number += 1
 
 
-if __name__ == "__main__":
-    import sys
 
-    app = QApplication(sys.argv)
-    mw = MainWindow()
-    mw.show()
-    sys.exit(app.exec())
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    myapp = MyWindow()
+    myapp.show()
+    sys.exit(app.exec_())
