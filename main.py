c8b63bd8c89cef9596e24deacf0c144b55d15599
Tihon49 <tihon49@gmail.com>
2020-11-25 17:24:54 +0300

    укоротил вывод строк в таблицу

--- main.py
+++ main.py
@@ -14,33 +14,17 @@ class MyWindow(QtWidgets.QMainWindow):
 
         # получаем данные для вывода в таблице
         self.data = show_full_data()
+        # устанавливаем кол-во строк в таблице
         self.ui.tableWidget.setRowCount(len(self.data))
 
-        # сначала создаем необходимое кол-во рядов согласно кол-ву полученных данных
-        for i in range(len(self.data)):
-            item = QtWidgets.QTableWidgetItem()
-            self.ui.tableWidget.setItem(i, 0, item)
-            item = QtWidgets.QTableWidgetItem()
-            self.ui.tableWidget.setItem(i, 1, item)
-            item = QtWidgets.QTableWidgetItem()
-            self.ui.tableWidget.setItem(i, 2, item)
-            item = QtWidgets.QTableWidgetItem()
-            self.ui.tableWidget.setItem(i, 3, item)
-
+        self.row_index = 0
         # теперь добавляем в каждую стоку полученные ранее данные
-        row_number = 0
         for agent, contract in self.data:
-            item = self.ui.tableWidget.item(row_number, 0)
-            item.setText(str(agent.id))
-            item = self.ui.tableWidget.item(row_number, 1)
-            item.setText(agent.name)
-            item = self.ui.tableWidget.item(row_number, 2)
-            item.setText(contract.number)
-            item = self.ui.tableWidget.item(row_number, 3)
-            item.setText(contract.description)
-            row_number += 1
-
-
+            self.ui.tableWidget.setItem(self.row_index, 0, QtWidgets.QTableWidgetItem(str(agent.id)))
+            self.ui.tableWidget.setItem(self.row_index, 1, QtWidgets.QTableWidgetItem(agent.name))
+            self.ui.tableWidget.setItem(self.row_index, 2, QtWidgets.QTableWidgetItem(contract.number))
+            self.ui.tableWidget.setItem(self.row_index, 3, QtWidgets.QTableWidgetItem(contract.description))
+            self.row_index += 1
 
 
 if __name__ == '__main__':
